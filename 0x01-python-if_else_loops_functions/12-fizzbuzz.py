24768f72c32fefa67c8d06638e29a7f95af10be4
Liyosi1996 <liyosifaith99@gmail.com>
2022-08-24 00:38:28 +0300

    FizzBuzz commit

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
     if i in range(1, 101):
-        if i % 3 == 0:
+        if i % 15 == 0:
+            print("FizzBuzz", end='')
+        elif i % 5 == 0:
+            print("Buzz", end='')
+        elif i % 3 == 0:
             print("Fizz", end='')
-            if i % 5 == 0:
-                print("Buzz", end='')
-                if i % 3 and i % 5:
-                    print("{:d}".format(i), end='')
-                    print(end=' ')
+        else:
+            print(i, end=" ")
