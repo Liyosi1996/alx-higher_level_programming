6a1921a685d2e52a62efa210d47bfb433ff1eaea
Liyosi1996 <liyosifaith99@gmail.com>
2022-08-25 21:46:04 +0300

    hidden discovery file

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] !="__":
-            print(name)
+    from hidden_4 import *
+    arr = dir()
+    for a in range(0, len(arr)):
+        if arr[a][0:2] != "__":
+            print("{}".format(arr[a]))
