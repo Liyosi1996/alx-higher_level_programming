4a63de9f7bb3e52c4089439c25dff562d8fa9d79
Liyosi1996 <liyosifaith99@gmail.com>
2022-08-26 00:18:59 +0300

    infinite_add file

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-
     import sys
-
     sum = 0
-        for j in range(1, len(sys.argv)):
-            sum += int(sys.argv[j])
-        print('{}'.format(sum))
+    for j in range(1, len(sys.argv)):
+        sum += int(sys.argv[j])
+    print('{}'.format(sum))
