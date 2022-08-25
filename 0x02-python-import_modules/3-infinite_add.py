f919e2948dd53c3a1551d798ee5d6c211e0a0513
Liyosi1996 <liyosifaith99@gmail.com>
2022-08-25 20:41:06 +0300

    addition of all args

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    sum = 0
+    if len(argv) > 1:
+        for j in range(1, len(argv)):
+            sum += int(argv[j])
+        print(sum)
