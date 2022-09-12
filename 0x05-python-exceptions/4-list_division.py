6dffe9a6aa2e467f8fc347d87e6dba44ed5deb89
Liyosi1996 <liyosifaith99@gmail.com>
2022-09-12 21:57:10 +0300

    divides element by element

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -16,5 +16,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
         finally:
             pass
-        div.append(return)
+        div.append(result)
     return div
